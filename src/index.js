4b6a589243a55c913f5b2c145087f6311abafbc6
Ahsan-Masood98 <leofinn6@gmail.com>
2022-05-22 16:38:00 +0500

    adding redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 //Redux Setup
-import { legacy_createStore as createStore } from "redux";
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 import rootReducers from "./reducers";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancer(applyMiddleware(thunk))
 );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>

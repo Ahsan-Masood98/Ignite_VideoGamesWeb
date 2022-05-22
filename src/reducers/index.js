847c0793f83cddee639964f7472938208574b093
Ahsan-Masood98 <leofinn6@gmail.com>
2022-05-22 16:20:24 +0500

    initializing redux

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import gamesReducer from "./gamesReducer";
+
+const rootReducers = combineReducers({
+  games: gamesReducer,
+});
+export default rootReducers;
